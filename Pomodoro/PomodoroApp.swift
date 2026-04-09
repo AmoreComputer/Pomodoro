4c189ff39196fcb5ee079f0bd7b862d9558c027c
Lucas Fischer <lucas.fischer@me.com>
2026-04-09 14:39:21 +0800

    add Sparkle

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -1,10 +1,24 @@
+import Sparkle
 import SwiftUI
 
 @main
 struct PomodoroApp: App {
+    private let updaterController = SPUStandardUpdaterController(
+        startingUpdater: true,
+        updaterDelegate: nil,
+        userDriverDelegate: nil
+    )
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .commands {
+            CommandGroup(after: .appInfo) {
+                Button("Check for Updates…") {
+                    updaterController.updater.checkForUpdates()
+                }
+            }
         }
     }
 }
