6a5aa32fabfa2d07cc20939eb9643315c059e7e6
Lucas Fischer <lucas.fischer@me.com>
2026-04-09 14:14:58 +0800

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Lucas Fischer on 09.04.26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
